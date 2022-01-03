bebbc11df0d9332716be51f53bf213c7cadf8afe
Kendall <kdmccroy@gmail.com>
2022-01-03 05:05:39 -0500

    worked on scraping and importing webdrivermanager

---
+++ venv/scrape_mars.py
@@ -0,0 +1,18 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import pandas as pd
+
+# NASA Mars
+def scrape():
+    # browser = init_browser()
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    url = "https://redplanetscience.com/"
+    browser.visit(url)
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.prettify())
