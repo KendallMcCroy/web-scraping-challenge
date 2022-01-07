4ebcfa1d0a10bc201d824c63d6ff089a321c7497
Kendall <kdmccroy@gmail.com>
2022-01-07 04:54:10 -0500

    fixed ipynb page

--- venv/app.py
+++ venv/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for 
 from flask_pymongo import PyMongo
 import scrape_mars
 
@@ -15,15 +15,15 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
-
+ 
 
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
-    mars.update_one({}, {"$set": mars_data}, upsert=True)
-    return redirect('/', code=302)
+    mars_data = scrape_mars.scrape()
+    mars.update_one({}, mars_data, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- venv/scrape_mars.py
+++ venv/scrape_mars.py
@@ -1,20 +1,39 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
-import requests
 import pandas as pd
+import datetime as dt
 
 # NASA Mars
 def scrape():
     # browser = init_browser()
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+  
+    news_title, news_paragraph = mars_news(browser)
 
-    mars = {}
+    data = {
+        "news-title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts()
+        # "hemispheres": hemispheres(browser), 
+        # "last_modified": dt.datetime.now()
+    }
+
+    browser.quit()
+    return data 
     
-    url = "https://redplanetscience.com/"
-    browser.visit(url)
+    # url = "https://redplanetscience.com/"
+    # browser.visit(url)
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup.prettify())
+    # html = browser.html
+    # soup = BeautifulSoup(html, "html.parser")
+ 
+    # # response = requests.get(url)
+    # # soup = BeautifulSoup(response.text, 'html.parser')
+    # print(soup.prettify())
+
+    # #Quit Brower.html
+    # browser.quit()
+    # return mars_data 
